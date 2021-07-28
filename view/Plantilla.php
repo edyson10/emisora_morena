f3c5a52a2076f980cc79ff0b1ee750882ebd3f51
edyson leal <edysonfabianlm@ufps.edu.co>
2021-07-28 14:27:24 -0500

    Se sube los cambios de la pagina.

--- view/Plantilla.php
+++ view/Plantilla.php
@@ -34,7 +34,7 @@ if (isset($_GET["ubicacion"])) {
         <script src="view/presentacion/js/alertas.js"></script>
         <script src="view/presentacion/js/sweetalert2.min.js"></script>
         <script src="view/presentacion/js/correo.js"></script>
-        
+
         <script>
             $(function() {
                 $('.tabgroup > div').hide();
@@ -101,6 +101,47 @@ if (isset($_GET["ubicacion"])) {
         </body>
 
         </html>
+    <?php } else if ($_GET["ubicacion"] == "Privado") {
+        include "modulos/navegacion/" . $_GET["ubicacion"] . ".php";
+    } else if ($_GET["ubicacion"] == "Administracion") { ?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>AdminLTE 3 | Dashboard</title>
+
+            <!-- Google Font: Source Sans Pro -->
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+            <!-- Font Awesome -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/fontawesome-free/css/all.min.css">
+            <!-- Ionicons -->
+            <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+            <!-- Tempusdominus Bootstrap 4 -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+            <!-- iCheck -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+            <!-- JQVMap -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/jqvmap/jqvmap.min.css">
+            <!-- Theme style -->
+            <link rel="stylesheet" href="view/presentacion/admin/dist/css/adminlte.min.css">
+            <!-- overlayScrollbars -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+            <!-- Daterange picker -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/daterangepicker/daterangepicker.css">
+            <!-- summernote -->
+            <link rel="stylesheet" href="view/presentacion/admin/plugins/summernote/summernote-bs4.min.css">
+        </head>
+
+        <body class="hold-transition sidebar-mini layout-fixed">
+            <div class="wrapper">
+                <?php 
+                    include_once "modulos/header.php";
+                    include_once "modulos/menuAdmin.php";
+                ?>
+            </div>
+        </body>
 <?php } else {
         header('location: Inicio');
     }
