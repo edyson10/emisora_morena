0371f71592409c45681a7b3343bdb87243af32a6
edyson leal <edysonfabianlm@ufps.edu.co>
2021-10-05 19:25:32 -0500

    Se modifico la conexion al servidor de produccion.

--- model/conexion.php
+++ model/conexion.php
@@ -4,10 +4,8 @@
 try {
     /* CONEXION CON EL SERVIDOR LOCAL PRODUCCION */
     //$conexion = mysqli_connect("localhost","root","","emisora");
-    /* CONEXION CON EL SERVIDOR AWS DESARROLLO */
-    $conexion = mysqli_connect("52.90.238.70","admin","edyson10","emisora");
     /* CONEXION CON EL SERVIDOR PRODUCCION */
-    //$conexion = mysqli_connect("173.201.178.160","turcolombia","turcolombia2021","turcolombia");
+    $conexion = mysqli_connect("localhost","genioweb1_admin","GJp.BLO{qcgm","genioweb1_emisora");
     //echo "Connected successfully";
 } catch (PDOException $error) {
     echo 'Connection error: ' . $error->getMessage();
