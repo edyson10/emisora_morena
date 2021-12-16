1c79e724faa7665033b6012204c978341f9fe61d
edyson leal <edysonfabianlm@ufps.edu.co>
2021-12-15 22:17:07 -0500

    Se modifico la conexion.

--- model/conexion.php
+++ model/conexion.php
@@ -3,9 +3,9 @@
 // Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
 try {
     /* CONEXION CON EL SERVIDOR LOCAL PRODUCCION */
-    $conexion = mysqli_connect("localhost","root","","emisora");
+    //$conexion = mysqli_connect("localhost","root","","emisora");
     /* CONEXION CON EL SERVIDOR PRODUCCION */
-    //$conexion = mysqli_connect("localhost","genioweb1_admin","GJp.BLO{qcgm","genioweb1_emisora");
+    $conexion = mysqli_connect("localhost","genioweb1_admin","GJp.BLO{qcgm","genioweb1_emisora");
     //echo "Connected successfully";
 } catch (PDOException $error) {
     echo 'Connection error: ' . $error->getMessage();
